fc6a3ad034f172e12b0401a1d67bacf21b4ec414
Taylor Ford <tford28@uncc.edu>
2020-10-20 23:24:31 -0400

    Instructions for Coding Exercise 04

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.post('/api/login', (req, res) => {
     
     for (let user of users){
         if (username == user.username && password == user.password) {
-            let token = jwt.sign({ id: user.id, username: user.username}, secretKey, {expiresIn: '180s'});
+            let token = jwt.sign({ id: user.id, username: user.username}, secretKey, {expiresIn: '3m'});
             res.json({
                 success: true,
                 err: null,
